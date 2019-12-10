c936e9ec7d432c9446beb7c22797582fbdb0ee04
Liam Pulsifer <lwpulsifer@gmail.com>
2019-12-10 17:16:46 -0500

    Latest changes to webapp

--- webapp/client/src/components/UserSearch.vue
+++ webapp/client/src/components/UserSearch.vue
@@ -1,7 +1,38 @@
 <template>
-  <div class="container">
-    <button type="button" class="btn btn-primary">{{ msg }}</button>
-  </div>
+  <b-container>
+    <div class="container">
+      <!-- <b-button v-b-modal.searchmodal> Search </b-button> -->
+        <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+        <b-form-group id="form-title-group"
+                      label="First Name"
+                      label-for="form-name-input">
+            <b-form-input id="form-first-name-input"
+                          type="text"
+                          v-model="searchForm.first_name"
+                          required
+                          placeholder="Enter first name">
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="form-name-group"
+                        label="Last name:"
+                        label-for="form-name-input">
+              <b-form-input id="form-last-name-input"
+                            type="text"
+                            v-model="searchForm.last_name"
+                            required
+                            placeholder="Enter last name">
+              </b-form-input>
+            </b-form-group>
+          <b-form-group id="form-read-group">
+            <b-form-checkbox-group v-model="searchForm.alive" id="form-checks">
+              <b-form-checkbox value="true">Alive?</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger" @reset="onReset">Reset</b-button>
+        </b-form>
+      </div>
+  </b-container>
 </template>
 
 <script>
@@ -12,23 +43,54 @@ export default {
   data() {
     return {
       msg: 'Stuff!',
+      searchForm: {
+        first_name: '',
+        last_name: '',
+        alive: [],
+      },
     };
   },
   methods: {
-    getMessage() {
-      const path = 'http://localhost:5000/ping';
-      axios.get(path)
-        .then((res) => {
-          this.msg = res.data;
+    initForm() {
+      this.searchForm.first_name = '';
+      this.searchForm.last_name = '';
+      this.searchForm.alive = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      const payload = {
+        first_name: this.searchForm.first_name,
+        last_name: this.searchForm.last_name,
+        alive: this.searchForm.alive, // property shorthand
+      };
+      this.sendSearch(payload);
+      this.initForm();
+    },
+    sendSearch(payload) {
+      const path = 'http://localhost:5000/search';
+      axios.post(path, payload)
+        .then((response) => {
+          // eslint-disable-next-line
+          console.log(response.data);
         })
         .catch((error) => {
           // eslint-disable-next-line
-          console.error(error);
+          console.log(error);
         });
     },
+    onReset(evt) {
+      evt.preventDefault();
+      this.initForm();
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+      // eslint-disable-next-line
+      console.log("Reset")
+    },
   },
-  created() {
-    this.getMessage();
-  },
+  // created() {
+  //   this.getMessage();
+  // },
 };
 </script>
--- webapp/server/app.py
+++ webapp/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 
@@ -17,6 +17,19 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+@app.route('/search', methods=['GET', 'POST'])
+def send_search():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        search_data = request.get_json()
+        first_name = search_data.get('first_name')
+        last_name = search_data.get('last_name')
+        alive = search_data.get('alive')
+        response_object['message'] = f'Searching for person with name {first_name} {last_name}... (Not implemented)'
+    else:
+        response_object['message'] = 'Nothing to get'
+    return jsonify(response_object)
 
 
 if __name__ == '__main__':
