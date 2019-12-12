89edeada3c0aba1ccac2fa63bd43e7764645dd17
Liam Pulsifer <lwpulsifer@gmail.com>
2019-12-12 16:37:39 -0500

    Removed api key

--- webapp/server/twitter_bot.py
+++
@@ -1,5 +0,0 @@
-import twitter
-api = twitter.Api(consumer_key="25jLINdVpEVgocvnoypcH1ISk",
-                  consumer_secret="zwJGt0Gpd32EDFlUgFkE0vRMW8e6g9ou6zKdOkiLEh28fMJLmB",
-                  access_token_key="1205160096134623232-X5LU2sMr7t6A5FX0m5O52kHpRUyh3o",
-                  access_token_secret="br1LhqAwJQ131xag5jz8o6hSrK18VJNhmTGzs1xMM5SFn")
