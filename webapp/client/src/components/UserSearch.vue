ab020f708b10580d9d99f41a740bbfe16f3177aa
Liam Pulsifer <lwpulsifer@gmail.com>
2019-12-12 12:43:27 -0500

    Changed to be a one-page paradigm, added middle name exclusion capability

--- webapp/client/src/components/UserSearch.vue
+++ webapp/client/src/components/UserSearch.vue
@@ -1,41 +1,61 @@
 <template>
   <b-container>
+  <b-container v-if="show_search">
     <div>
       <h1> Find out what data of yours are floating around on the internet
       </h1>
     </div><br>
     <div class="container">
       <!-- <b-button v-b-modal.searchmodal> Search </b-button> -->
-        <b-form @submit="onSubmit" @reset="onReset" class="w-100">
-        <b-form-group id="form-title-group"
-                      label="First Name"
+        <b-form @submit="onSubmit" class="w-100">
+        <b-input-group prepend="Name"
+                      id="form-title-group"
+                      label="Name"
                       label-for="form-name-input">
             <b-form-input id="form-first-name-input"
                           type="text"
                           v-model="searchForm.first_name"
-                          required
-                          placeholder="Enter first name">
+                          placeholder="Enter first name (Optional)">
             </b-form-input>
-        </b-form-group>
-        <b-form-group id="form-name-group"
-                        label="Last name:"
-                        label-for="form-name-input">
-          <b-form-input id="form-last-name-input"
+            <b-form-input id="form-middle-name-input"
+                            type="text"
+                            v-model="searchForm.middle_initial"
+                            placeholder="MI">
+            </b-form-input>
+            <b-form-input id="form-last-name-input"
                             type="text"
                             v-model="searchForm.last_name"
                             required
                             placeholder="Enter last name">
+            </b-form-input>
+        </b-input-group>
+        <b-input-group prepend="Twitter Handle">
+          <b-form-input id="form-handle-input"
+                              type="text"
+                              v-model="searchForm.handle"
+                              placeholder="Enter twitter handle (optional)">
           </b-form-input>
-        </b-form-group>
+        </b-input-group>
+        <br>
           <b-button type="submit" variant="primary">Go</b-button>
-          <b-button type="reset" variant="danger" @reset="onReset">Reset</b-button>
         </b-form>
       </div>
+  </b-container>
+  <b-container v-if="!show_search">
+      <h1>Voter Data</h1>
+      <b-table striped hover :items='voter_data'></b-table>
+    <br>
+    <div class="back-to-search">
+      <b-button v-on:click="toggleSearch"> Return to Search </b-button>
+    </div>
+  </b-container>
   </b-container>
 </template>
 
 <script>
 import axios from 'axios';
+
+const NProgress = require('nprogress');
 
 export default {
   name: 'UserSearch',
@@ -45,44 +65,61 @@ export default {
       searchForm: {
         first_name: '',
         last_name: '',
+        middle_initial: '',
+        handle: '',
       },
+      show_search: true,
+      voter_data: [],
     };
   },
   methods: {
     initForm() {
       this.searchForm.first_name = '';
       this.searchForm.last_name = '';
+      this.searchForm.middle_initial = '';
+      this.handle = '';
     },
     onSubmit(evt) {
       evt.preventDefault();
       const payload = {
         first_name: this.searchForm.first_name,
         last_name: this.searchForm.last_name,
+        middle_initial: this.searchForm.middle_initial,
+        handle: this.handle,
       };
       this.sendSearch(payload);
     },
     sendSearch(payload) {
       const path = 'http://localhost:5000/search';
+      NProgress.start();
       axios.post(path, payload)
         .then((response) => {
           // eslint-disable-next-line
           console.log(response.data.search_result);
-          this.$router.push({ name: 'SearchResults' });
+          NProgress.done();
+          this.show_search = false;
+          this.getMessage();
+          // this.$router.push({ name: 'SearchResults' });
         })
         .catch((error) => {
           // eslint-disable-next-line
           console.log(error);
         });
     },
-    onReset(evt) {
-      evt.preventDefault();
-      this.initForm();
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
-      // eslint-disable-next-line
-      console.log("Reset")
+    getMessage() {
+      const path = 'http://localhost:5000/results';
+      axios.get(path)
+        .then((res) => {
+          this.voter_data = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    toggleSearch() {
+      this.show_search = true;
+      console.log(this.show_search);
     },
   },
   // created() {
--- webapp/server/voter_data_search.py
+++ webapp/server/voter_data_search.py
@@ -3,7 +3,7 @@ import csv
 
 class VoterDataSearch():
 
-    def __init__(self, prefix='data', postfix="-voter-data.tsv"):
+    def __init__(self, prefix='server/data', postfix="-voter-data.tsv"):
         self.prefix = prefix
         self.postfix = postfix
     
@@ -13,7 +13,7 @@ class VoterDataSearch():
         and returns a file path of the form
         prefix/{first character of last_name}-voter-data.tsv
         '''
-        return os.path.join(self.prefix, last_name.lower()[0] + self.postfix)
+        return os.path.join(os.getcwd(), self.prefix, last_name.lower()[0] + self.postfix)
 
     def process_line(self, line):
         '''
@@ -25,14 +25,24 @@ class VoterDataSearch():
         val_dict = {header[x]: line[x] for x in range(len(line))}
         return val_dict
     
-    def search(self, first_name="", last_name="", index=9):
+    def checkLine(self, line, index, last_name, first_name, mi):
+        if line[index].strip().lower() == last_name.lower() and \
+            line[index + 1].strip().lower() == first_name.lower():
+            if mi and line[index + 2].strip().lower().startswith(mi.lower()):
+                return True
+            if not mi:
+                return True
+        return False
+
+    def search(self, first_name="", last_name="", mi="", index=9):
         path = self.getPath(last_name)
+        # matches = []
         with open(path, 'r') as filepath:
             reader = csv.reader(filepath, delimiter='\t')
             for line in reader:
-                if line[index].lower() == last_name.lower() and line[index + 1].lower() == first_name.lower():
+                if self.checkLine(line, index, last_name, first_name, mi):
                     return True, self.process_line(line)
-        return False, "Person not found"
+        return False, {"Person not found": "ERROR"}
 
 if __name__ == '__main__':
     search = VoterDataSearch()
