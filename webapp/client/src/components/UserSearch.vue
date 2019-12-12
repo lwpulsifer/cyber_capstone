312df33bc2acd4f14f5fd1505bfccfdf9da20d8d
Liam Pulsifer <lwpulsifer@gmail.com>
2019-12-12 16:19:25 -0500

    A little more twitter backend server stuff and all of the frontend for it

--- webapp/client/src/components/UserSearch.vue
+++ webapp/client/src/components/UserSearch.vue
@@ -42,9 +42,33 @@
       </div>
   </b-container>
   <b-container v-if="!show_search">
-      <h1>Voter Data</h1>
-      <b-table striped hover :items='voter_data'></b-table>
+    <b-row>
+      <b-col>
+        <h1>Voter Data for</h1>
+        <h2 color='red'>{{ first_name }} {{ last_name }}</h2>
+      </b-col>
+      <b-col>
+        <h1> Twitter Activity </h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+      <b-table class='tables'
+          sticky-header='400px'
+          bordered striped hover
+          :items='voter_data'></b-table>
+      </b-col>
     <br>
+    <b-col>
+      <!-- <b-card v-for="tweet in tweets" :key="tweet">
+        {{ tweet }}
+      </b-card> -->
+      <b-table class='tables'
+          sticky-header='400px'
+          bordered striped hover
+          :items='tweets'></b-table>
+    </b-col>
+    </b-row>
     <div class="back-to-search">
       <b-button v-on:click="toggleSearch"> Return to Search </b-button>
     </div>
@@ -54,11 +78,15 @@
 
 <script>
 import axios from 'axios';
+// import { Tweet } from 'vue-tweet-embed';
 
 const NProgress = require('nprogress');
 
 export default {
   name: 'UserSearch',
+  // components: {
+  //   Tweet,
+  // },
   data() {
     return {
       msg: 'Stuff!',
@@ -68,8 +96,11 @@ export default {
         middle_initial: '',
         handle: '',
       },
+      first_name: '',
+      last_name: '',
       show_search: true,
       voter_data: [],
+      tweets: [],
     };
   },
   methods: {
@@ -85,8 +116,10 @@ export default {
         first_name: this.searchForm.first_name,
         last_name: this.searchForm.last_name,
         middle_initial: this.searchForm.middle_initial,
-        handle: this.handle,
+        handle: this.searchForm.handle,
       };
+      this.first_name = this.searchForm.first_name;
+      this.last_name = this.searchForm.last_name;
       this.sendSearch(payload);
     },
     sendSearch(payload) {
@@ -99,7 +132,7 @@ export default {
           NProgress.done();
           this.show_search = false;
           this.getMessage();
-          // this.$router.push({ name: 'SearchResults' });
+          this.getTweets();
         })
         .catch((error) => {
           // eslint-disable-next-line
@@ -117,9 +150,21 @@ export default {
           console.error(error);
         });
     },
+    getTweets() {
+      const path = 'http://localhost:5000/tweets';
+      axios.get(path)
+        .then((res) => {
+          this.tweets = res.data;
+          // eslint-disable-next-line
+          console.log(this.tweets)
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error)
+        });
+    },
     toggleSearch() {
       this.show_search = true;
-      console.log(this.show_search);
     },
   },
   // created() {
--- webapp/client/src/main.js
+++ webapp/client/src/main.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
