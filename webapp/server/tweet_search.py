6cb1561d0f94f6e29faf95408c63e579055505d9
Liam Pulsifer <lwpulsifer@gmail.com>
2019-12-12 19:19:42 -0500

    Added error checking to Twitter username search

--- webapp/server/tweet_search.py
+++ webapp/server/tweet_search.py
@@ -1,4 +1,5 @@
 from twitter_bot import api as default_api
+from twitter import TwitterError
 
 class TweetSearch():
 
@@ -6,12 +7,16 @@ class TweetSearch():
         self.api = api
         self.user = user
         self.count = count
+        self.error = [{"ERROR": f"Twitter user <{self.user}\> is invalid. Try again."}]
     
     def get_tweets(self, api=None, screen_name=None, count=None):
         if not api: api = self.api
         if not screen_name: screen_name = self.user
         if not count: count = self.count
-        timeline = api.GetUserTimeline(screen_name=screen_name, count=count)
+        try:
+            timeline = api.GetUserTimeline(screen_name=screen_name, count=count)
+        except TwitterError as e:
+            return None
         return timeline
     
     def get_ids(self, api=None, screen_name=None, count=None):
@@ -20,8 +25,11 @@ class TweetSearch():
         if screen_name is None: screen_name = self.user
         if count is None: count = self.count
         ids = []
-        for tweet in self.get_tweets():
-            ids.append({"text": tweet.text, "created at": tweet.created_at, "favorites": tweet.favorite_count})
+        if self.get_tweets():
+            for tweet in self.get_tweets():
+                ids.append({"text": tweet.text, "created at": tweet.created_at, "favorites": tweet.favorite_count})
+        else:
+            ids = self.error
         return ids
 
 if __name__ == '__main__':
